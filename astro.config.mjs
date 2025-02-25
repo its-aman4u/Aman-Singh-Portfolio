5616a3e587bba091e7a5c4549f70a4bc5aff7eed
its-aman4u <aman.singh01031997@gmail.com>
2025-02-25 13:21:50 +0530

    chore: update Vercel and Astro configuration for static deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,13 @@
 import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import netlify from '@astrojs/netlify';
+import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/static';
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    react({
-      include: ['**/react/*', '**/components/**/*.{jsx,tsx}'],
-    }),
-    tailwind(),
-    netlify(),
-  ],
-  output: 'server',
-  adapter: netlify(),
+  integrations: [tailwind(), react()],
+  output: 'static',
+  adapter: vercel({
+    analytics: true,
+  }),
 });
